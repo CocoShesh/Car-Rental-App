e2be31a57203548a492594e55af14ce0ebdbcc7b
NARD <glenardrefamonte1@gmail.com>
2024-03-13 17:37:57 +0800

    fix error in image displayed

--- src/Pages/OurTeam/data.jsx
+++ src/Pages/OurTeam/data.jsx
@@ -3,38 +3,38 @@ const employees = [
     id: 1,
     Name: "Luke Miller",
     Position: "Salesman",
-    src: "/public/images/Salesman.png",
+    src: "/images/Salesman.png",
   },
   {
     id: 2,
     Name: "Michael Diaz",
     Position: "Business Owner",
-    src: "/public/images/Business Owner.png",
+    src: "/images/Business Owner.png",
   },
   {
     id: 3,
     Name: "Briana Ross",
     Position: "Photographer",
-    src: "/public/images/Photographer.png",
+    src: "/images/Photographer.png",
   },
   {
     id: 4,
     Name: "Lauren Rivera",
     Position: "Car Detailist",
-    src: "/public/images/Car Detailist.png",
+    src: "/images/Car Detailist.png",
   },
   {
     id: 5,
     Name: "Martin Rizz",
     Position: "Mechanic",
-    src: "/public/images/Mechanic.png",
+    src: "/images/Mechanic.png",
   },
   {
     id: 6,
     Name: "Caitlyn Hunt",
     Position: "Manager",
     // Add src for Manager if applicable
-    src: "/public/images/Manager.png",
+    src: "/images/Manager.png",
   },
 ];
 
