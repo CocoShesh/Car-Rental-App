af3f93a9e086942fa0c8fd558b87000176657be4
NARD <glenardrefamonte1@gmail.com>
2024-04-17 16:33:03 +0800

    fix the width

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Banner = () => {
   return (
     <>
-      <section className="bg-[#2d2d2d] relative z-10 w-auto h-[250px] mb-20 flex flex-col items-center text-center justify-center max-sm:w-full max-sm:pl-5 max-sm:h-auto max-sm:text-center  max-sm:p-10">
+      <section className="bg-[#2d2d2d] relative z-10 w-full h-[250px] mb-20 flex flex-col items-center text-center justify-center max-sm:w-full max-sm:pl-5 max-sm:h-auto max-sm:text-center  max-sm:p-10">
         <h1 className="text-white text-[52px] mb-2 max-sm:text-[30px] ">
           Save big with our cheap car rental!
         </h1>
