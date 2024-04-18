888f0a8e8f43325ae17655998032d00353ef8eff
NARD <glenardrefamonte1@gmail.com>
2024-04-19 00:02:07 +0800

    fix the text size

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,10 +4,10 @@ const Banner = () => {
   return (
     <>
       <section className="bg-[#2d2d2d] relative z-10 w-full h-[250px] mb-20 flex flex-col items-center text-center justify-center max-sm:w-full max-sm:pl-5 max-sm:h-auto max-sm:text-center  max-sm:p-10">
-        <h1 className="text-white text-[52px] mb-2 max-sm:text-[30px] ">
+        <h1 className="text-white text-[52px] mb-2 max-sm:text-[34px] ">
           Save big with our cheap car rental!
         </h1>
-        <p className="text-white text-[24px]">
+        <p className="text-white text-[24px] max-sm:text-lg">
           Top Airports. Local Suppliers.{" "}
           <span className="text-orange-500">24/7 </span>Support.
         </p>
