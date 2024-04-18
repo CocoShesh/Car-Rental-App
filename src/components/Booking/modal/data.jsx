770173ba27951d5626581690ec03e19215ba6705
NARD <glenardrefamonte1@gmail.com>
2024-04-18 14:05:53 +0800

    fix error in displaying images

--- src/components/Booking/modal/data.jsx
+++ src/components/Booking/modal/data.jsx
@@ -1,10 +1,10 @@
 const Data = [
-  { carName: "Audi A1S-Line", src: "/public/images/Audi.jpg" },
-  { carName: "Vw Golf 6", src: "/public/images/Golf.jpg" },
-  { carName: "Toyota Camry", src: "/public/images/toyotacamry.jpg" },
-  { carName: " BMW 320 ModernLine", src: "/public/images/MorderLine.jpg" },
-  { carName: "Mercedes-Benz GLk", src: "/public/images/Mercedes.jpg" },
+  { carName: "Audi A1S-Line", src: "/images/Audi.jpg" },
+  { carName: "Vw Golf 6", src: "/images/Golf.jpg" },
+  { carName: "Toyota Camry", src: "/images/toyotacamry.jpg" },
+  { carName: " BMW 320 ModernLine", src: "/images/MorderLine.jpg" },
+  { carName: "Mercedes-Benz GLk", src: "/images/Mercedes.jpg" },
 
-  { carName: "VW Passat CC", src: "/public/images/Passat.jpg" },
+  { carName: "VW Passat CC", src: "/images/Passat.jpg" },
 ];
 export default Data;
