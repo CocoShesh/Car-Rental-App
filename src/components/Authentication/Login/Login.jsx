68299b73d8e8e2875a937b0106bab285b6d35b6e
NARD <110368170+CocoShesh@users.noreply.github.com>
2024-04-20 03:55:31 +0800

    Update Login.jsx

--- src/components/Authentication/Login/Login.jsx
+++ src/components/Authentication/Login/Login.jsx
@@ -12,7 +12,7 @@ const Login = ({ handleOpen }) => {
   return (
     <>
       <section className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-10 bg-[#0000004b] text-black">
-        <section className="w-[60rem] h-[38rem]  border bg-white rounded-2xl p-10 flex gap-10">
+        <section className="w-[60rem] h-[38rem]  border bg-white rounded-2xl p-10 max-sm:p-0 flex gap-10">
           <section className="  bg-login bg-no-repeat h-full  bg-cover w-full max-lg:hidden "></section>
           <section className="   w-full px-5 flex flex-col gap-5 ">
             <section className="flex justify-between">
