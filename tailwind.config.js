ced2dddab5a205a11a2fe3a01ccc98853996eb22
NARD <glenardrefamonte1@gmail.com>
2024-04-20 02:15:15 +0800

    Added login illustrations image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         xs: "359px",
         "3xl": "2000px",
       },
+      backgroundImage: {
+        login: "url('/images/login-illustrator.png')",
+      },
     },
   },
   plugins: [require("daisyui")],
