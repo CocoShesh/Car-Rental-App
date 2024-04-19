3bcca61d3d82a3fb4d737a5bc61927069e81ab1c
NARD <glenardrefamonte1@gmail.com>
2024-04-20 02:16:33 +0800

    Created a register design

--- src/components/Authentication/Register/Register.jsx
+++ src/components/Authentication/Register/Register.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <section className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-[#0000004b]">
+        <section className="w-[60rem] h-[60rem] border bg-white">
+          fdsfdsasfaf
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default Register;
