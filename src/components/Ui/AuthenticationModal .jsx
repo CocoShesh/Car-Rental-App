314ca5b9f75a8cf41276724a1ce64f978391eb74
NARD <glenardrefamonte1@gmail.com>
2024-04-21 00:36:15 +0800

    FIx the width for large screen

--- src/components/Ui/AuthenticationModal .jsx
+++ src/components/Ui/AuthenticationModal .jsx
@@ -10,9 +10,9 @@ const AuthenticationModal = ({ children }) => {
       <section className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-10 bg-[#0000004b] text-black">
         <section
           data-aos="zoom-in"
-          className="w-[60rem] h-[38rem] max-sm:h-fit  border bg-white rounded-2xl p-10  max-sm:px-1 flex gap-10"
+          className="w-[60rem] h-[38rem] max-sm:h-fit  2xl:h-fit 2xl:w-[75rem] border bg-white rounded-2xl p-10  max-sm:px-1 flex gap-10"
         >
-          <section className="  bg-login bg-no-repeat h-full  max-sm:h-fit bg-cover w-full max-lg:hidden "></section>
+          <section className="  bg-login bg-no-repeat h-full  max-sm:h-fit 2xl:h-auto bg-cover w-full max-lg:hidden "></section>
           {children}
         </section>
       </section>
