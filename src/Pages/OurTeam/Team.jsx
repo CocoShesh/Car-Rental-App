c028e74f3e373ca96f969117398933f971f8b1dc
NARD <glenardrefamonte1@gmail.com>
2024-04-21 00:36:39 +0800

    Fix the width for large screen

--- src/Pages/OurTeam/Team.jsx
+++ src/Pages/OurTeam/Team.jsx
@@ -17,30 +17,32 @@ const Team = () => {
           </p>
         </section>
       </HeaderPage>
-      <section className="grid lg:grid-cols-3  lg:px-20 gap-10 w-full bg-white pt-32 sm:grid-cols-1 md:grid-cols-2 place-content-center sm:px-3">
-        {employees.map(employee => {
-          return (
-            <section
-              key={employee.id}
-              className="shadow-lg flex flex-col items-center justify-center w-auto rounded-t-lg"
-            >
-              <div className="">
-                <img
-                  src={employee.src}
-                  alt={employee.Name}
-                  className=" object-cover rounded-full"
-                />
-              </div>
-              <div className="text-center pt-5 py-10 text-black bg-white rounded-b-xl ">
-                <h1 className="text-[25px]"> {employee.Name}</h1>
-                <p className="text-[16px] text-[#777777] font-[Poppins]">
-                  {" "}
-                  {employee.Position}
-                </p>
-              </div>
-            </section>
-          );
-        })}
+      <section className="flex items-center justify-center">
+        <section className="grid lg:grid-cols-3  lg:px-20 gap-10   2xl:w-[1600px]  w-full bg-white pt-32 sm:grid-cols-1 md:grid-cols-2 place-content-center sm:px-3">
+          {employees.map(employee => {
+            return (
+              <section
+                key={employee.id}
+                className="shadow-lg flex flex-col items-center justify-center w-auto rounded-t-lg"
+              >
+                <div className="">
+                  <img
+                    src={employee.src}
+                    alt={employee.Name}
+                    className=" object-cover rounded-full"
+                  />
+                </div>
+                <div className="text-center pt-5 py-10 text-black bg-white rounded-b-xl ">
+                  <h1 className="text-[25px]"> {employee.Name}</h1>
+                  <p className="text-[16px] text-[#777777] font-[Poppins]">
+                    {" "}
+                    {employee.Position}
+                  </p>
+                </div>
+              </section>
+            );
+          })}
+        </section>
       </section>
     </>
   );
