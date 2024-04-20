704badb08db67d7f57651c6970fe03c2e9fd97f5
NARD <glenardrefamonte1@gmail.com>
2024-04-21 00:25:53 +0800

    Change the bg color

--- src/Pages/header/HeaderPage.jsx
+++ src/Pages/header/HeaderPage.jsx
@@ -4,7 +4,7 @@ const HeaderPage = ({ children }) => {
   return (
     <>
       <section className=" relative  h-[250px] w-full  bg-header-overlay bg-cover">
-        <section className="absolute  z-10 top-0 left-0 w-full h-[250px]   bg-[#fef4eeee]"></section>
+        <section className="absolute  z-10 top-0 left-0 w-full h-[250px]   bg-[#f3f3f3d8]"></section>
         <section className=" absolute z-20 top-0 w-full flex items-center justify-center ">
           <Header />
           {children}
