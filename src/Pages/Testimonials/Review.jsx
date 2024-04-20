9be2981fe0b54920781a67e83b33b821515299c3
NARD <glenardrefamonte1@gmail.com>
2024-04-21 00:11:07 +0800

    Refactor: change the code structure

--- src/Pages/Testimonials/Review.jsx
+++ src/Pages/Testimonials/Review.jsx
@@ -4,18 +4,19 @@ import Testimonials from "../../components/testimonials/Testimonials";
 const Review = () => {
   return (
     <>
-      <HeaderPage />
-      <section className="absolute  mx-10 top-[180px] z-10 ">
-        <h1 className="text-[42px] ">Testimonials</h1>
-        <p>
-          <a href="/">
-            <span className="hover:text-orange-500 ease-in-out transition duration-500 cursor-pointer">
-              Home
-            </span>
-          </a>
-          <span> / </span> Testimonials
-        </p>
-      </section>
+      <HeaderPage>
+        <section className="absolute  mx-10 top-[140px] 2xl:w-[1500px]  w-full px-8 ">
+          <h1 className="text-[42px] ">Testimonials</h1>
+          <p>
+            <a href="/">
+              <span className="hover:text-orange-500 ease-in-out transition duration-500 cursor-pointer">
+                Home
+              </span>
+            </a>
+            <span> / </span> Testimonials
+          </p>
+        </section>
+      </HeaderPage>
       <Testimonials />
     </>
   );
