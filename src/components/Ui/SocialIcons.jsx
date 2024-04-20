021875856da11cfb2a055277e5c9d77ed87d1579
NARD <glenardrefamonte1@gmail.com>
2024-04-20 16:48:58 +0800

    Feat: Created resuable ui for social icons

---
+++ src/components/Ui/SocialIcons.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+const icons = [
+  "/logos/facebook.png",
+  "/logos/instagram.png",
+  "/logos/gmail.png",
+  "/logos/tik-tok.png",
+];
+const SocialIcons = () => {
+  return (
+    <section className="flex gap-10 items-center justify-center">
+      {icons.map((icon, index) => (
+        <section
+          key={index}
+          className=" p-2 rounded-xl h-fit w-fit border border-[#81dbe12f] shadow cursor-pointer"
+        >
+          <img
+            src={icon}
+            alt={`Social Media Icon ${index}`}
+            className="w-8 h-8"
+          />
+        </section>
+      ))}
+    </section>
+  );
+};
+
+export default SocialIcons;
