52207c2990bb9250c1e8e72e17c9931b28e7ed10
NARD <glenardrefamonte1@gmail.com>
2024-04-20 16:47:06 +0800

    Refactor: Chnage the code structure

--- src/components/Authentication/Register/Register.jsx
+++ src/components/Authentication/Register/Register.jsx
@@ -1,13 +1,60 @@
 import React from "react";
-
-const Register = () => {
+import { IoIosClose } from "react-icons/io";
+import { MdOutlineAccountCircle, MdOutlineLockPerson } from "react-icons/md";
+import AuthenticationModal from "../../Ui/AuthenticationModal ";
+import InputField from "../../Ui/InputField";
+import SocialIcons from "../../Ui/SocialIcons";
+const Register = ({ handleOpenRegister, handleOpenLogin }) => {
   return (
     <>
-      <section className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-[#0000004b]">
-        <section className="w-[60rem] h-[60rem] border bg-white">
-          fdsfdsasfaf
+      <AuthenticationModal>
+        <section className="w-full px-5 flex flex-col gap-5 overflow-scroll">
+          <section className="flex justify-between">
+            <img
+              src="https://car-rental-ten.vercel.app/static/media/logo.0ad964cb93ab30cc809d.png"
+              alt=""
+              className="w-[100px]"
+            />
+            <IoIosClose
+              onClick={handleOpenRegister}
+              className="text-2xl cursor-pointer hover:text-[#ff4c30]"
+            />
+          </section>
+          <section className="mt-5">
+            <h1 className="text-2xl font-bold">Register</h1>
+            <h2 className="text-[#686464]">
+              Please enter Register details below
+            </h2>
+          </section>
+          <InputField type="text" placeholder="FullName" icon="email" />
+          <InputField type="email" placeholder="Email" icon="email" />
+          <InputField type="password" placeholder="Password" icon="password" />
+          <InputField
+            type="password"
+            placeholder="Confirm Password"
+            icon="password"
+          />
+          <button className="w-full h-fit py-3 bg-[#ff4e33] text-white text-xl font-medium rounded-lg hover:bg-[#ff4e33d3]">
+            Register
+          </button>
+          <span className="text-center text-sm mt-[-5px] text-[#686464a6]">
+            Already have an account?{" "}
+            <span
+              className="text-[#007bfc] underline cursor-pointer"
+              onClick={() => {
+                handleOpenLogin();
+                handleOpenRegister(false);
+              }}
+            >
+              Login
+            </span>
+          </span>
+          <span className="text-center text-sm text-[#686464a6]">
+            Or Register With
+          </span>
+          <SocialIcons />
         </section>
-      </section>
+      </AuthenticationModal>
     </>
   );
 };
