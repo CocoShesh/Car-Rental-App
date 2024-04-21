ad143859ac98cf3a6c32269ed6a6c73eb72d4db7
NARD <glenardrefamonte1@gmail.com>
2024-04-22 00:13:05 +0800

    Change the file extension to js

---
+++ src/Pages/OurTeam/data.js
@@ -0,0 +1,41 @@
+const employees = [
+  {
+    id: 1,
+    Name: "Luke Miller",
+    Position: "Salesman",
+    src: "/images/Salesman.png",
+  },
+  {
+    id: 2,
+    Name: "Michael Diaz",
+    Position: "Business Owner",
+    src: "/images/Business Owner.png",
+  },
+  {
+    id: 3,
+    Name: "Briana Ross",
+    Position: "Photographer",
+    src: "/images/Photographer.png",
+  },
+  {
+    id: 4,
+    Name: "Lauren Rivera",
+    Position: "Car Detailist",
+    src: "/images/Car Detailist.png",
+  },
+  {
+    id: 5,
+    Name: "Martin Rizz",
+    Position: "Mechanic",
+    src: "/images/Mechanic.png",
+  },
+  {
+    id: 6,
+    Name: "Caitlyn Hunt",
+    Position: "Manager",
+    // Add src for Manager if applicable
+    src: "/images/Manager.png",
+  },
+];
+
+export default employees;
