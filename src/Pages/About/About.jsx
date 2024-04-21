2b10139d75117996d801e59a64d6d116c8e45dd6
NARD <glenardrefamonte1@gmail.com>
2024-04-22 00:11:50 +0800

    Feat: Added information for about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Plan from "../../components/plan-section/Plan";
 import HeaderPage from "../header/HeaderPage";
+import { Link } from "react-router-dom";
 const About = () => {
   return (
     <>
@@ -8,16 +9,61 @@ const About = () => {
         <section className="absolute  mx-10 top-[140px]  2xl:w-[1500px] w-full px-8">
           <h1 className="text-[42px] "> About</h1>
           <p>
-            <a href="/">
+            <Link to="/">
               <span className="hover:text-orange-500 ease-in-out transition duration-500 cursor-pointer">
                 Home
               </span>
-            </a>
+            </Link>
             <span> / </span> About
           </p>
         </section>
       </HeaderPage>
 
+      <section className="flex  items-center justify-center  w-full h-[500px] max-lg:h-full  mt-10  ">
+        <section className="flex items-center justify-center mx-36  max-lg:flex-col   max-lg:w-full  max-md:px-3    gap-10 2xl:w-[1200px]">
+          <img
+            src="	https://car-rental-ten.vercel.app/static/media/about-main.ed0137841dc67d61ec95.jpg"
+            alt=""
+            className="w-fit h-full object-contain "
+          />
+          <section className="w-full h-full text-black  pr-10  ">
+            <h2 className="text-[22px]">About Company</h2>
+            <h1 className="text-5xl leading-[3.5rem] font-[600]">
+              You start the engine and your adventure begins
+            </h1>
+            <p className="text-[#866f7b] mt-5">
+              Certain but she but shyness why cottage. Guy the put instrument
+              sir entreaties affronting. Pretended exquisite see cordially the
+              you. Weeks quiet do vexed or whose. Motionless if no to affronting
+              imprudence no precaution. My indulged as disposal strongly
+              attended.
+            </p>
+            <section className="flex gap-10 mt-10 ">
+              <section>
+                <img src="/public/images/broom.png" alt="" />
+                <section className="flex items-center gap-4 mt-5">
+                  <h1 className="text-[42px]"> 20</h1>
+                  <span className="text-[#706f83] ">Car Types</span>
+                </section>
+              </section>
+              <section>
+                <img src="/public/images/tower.png" alt="" />
+                <section className="flex items-center gap-4 mt-5">
+                  <h1 className="text-[42px]">85</h1>
+                  <span className="text-[#706f83] ">Rental Outlets</span>
+                </section>
+              </section>
+              <section>
+                <img src="/public/images/parking.png" alt="" />
+                <section className="flex items-center gap-4 mt-5">
+                  <h1 className="text-[42px]">75</h1>
+                  <p className="text-[#706f7b] ">Repair Shop</p>
+                </section>
+              </section>
+            </section>
+          </section>
+        </section>
+      </section>
       {/* <section className="lg:px-10 justify-center items-center flex  w-full  gap-10 bg-white  h-fit pt-44  max-md:flex-col max-md:w-full  max-md:items-center max-md:justify-center">
         <img
           src="	https://car-rental-ten.vercel.app/static/media/about-main.ed0137841dc67d61ec95.jpg"
@@ -62,7 +108,7 @@ const About = () => {
           </section>
         </section>
       </section> */}
-      <section className="pt-44">
+      <section className="">
         <Plan />
       </section>
     </>
