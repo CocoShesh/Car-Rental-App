46222de7aeb80913e5017f0355a5e358babc5814
NARD <glenardrefamonte1@gmail.com>
2024-04-22 00:14:56 +0800

    Refactor: Changed the anchor tag to a link.

--- src/Pages/Testimonials/Review.jsx
+++ src/Pages/Testimonials/Review.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeaderPage from "../header/HeaderPage";
 import Testimonials from "../../components/testimonials/Testimonials";
+import { Link } from "react-router-dom";
 const Review = () => {
   return (
     <>
@@ -8,11 +9,11 @@ const Review = () => {
         <section className="absolute  mx-10 top-[140px] 2xl:w-[1500px]  w-full px-8 ">
           <h1 className="text-[42px] ">Testimonials</h1>
           <p>
-            <a href="/">
+            <Link to="/">
               <span className="hover:text-orange-500 ease-in-out transition duration-500 cursor-pointer">
                 Home
               </span>
-            </a>
+            </Link>
             <span> / </span> Testimonials
           </p>
         </section>
