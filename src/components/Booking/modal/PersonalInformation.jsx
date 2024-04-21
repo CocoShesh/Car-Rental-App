565a5cd5fcf639562e79ec77374da0d86d1410e6
NARD <glenardrefamonte1@gmail.com>
2024-04-22 00:10:09 +0800

    Feat: Created a component for the form where users input their personal information for car reservations.

--- src/components/Booking/modal/PersonalInformation.jsx
+++ src/components/Booking/modal/PersonalInformation.jsx
@@ -1,127 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { personalInfoSchema } from "../../../Schema/PersonalInfoSchema";
+import { useForm } from "react-hook-form";
+import { inputFields } from "../CarData";
+import InputField from "../InputField";
+import CompleteReservation from "./CompleteReservation";
+const PersonalInformation = ({ reservationDetails }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const [userInformation, setUserInformation] = useState([{}]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(personalInfoSchema),
+  });
 
-const PersonalInformation = () => {
-  const message = [
-    <span className="text-[10px] text-black">This field is required </span>,
-  ];
+  const onSubmit = data => {
+    setUserInformation(data);
+    setOpenModal(true);
+  };
+
+  const handleModal = () => {
+    setOpenModal(prev => !prev);
+  };
+
   return (
     <>
-      <section className="px-5">
-        <h1 className="text-[#ff4d30] font-extrabold text-[18px] uppercase mt-2">
-          Personal Information
-        </h1>
-        <section>
-          <section className="grid grid-cols-2 max-sm:grid-cols-1 mt-5 text-[#777777] text-[16px] font-bold gap-5">
-            <section>
-              <label htmlFor="Fname" className="">
-                First Name *
-              </label>{" "}
-              <br />
-              <input
-                type="text"
-                name=""
-                id="Fname"
-                placeholder="Enter your first name"
-                className="w-full h-[50px] max-sm:placeholder:text-xs bg-[#dbdbdb] pl-5"
-              />
-              <br />
-              {message}
-            </section>
-            <section>
-              <label htmlFor="Lname">Last Name *</label> <br />
-              <input
-                type="text"
-                name=""
-                id="Lname"
-                placeholder="Enter your last name"
-                className="w-full h-[50px] max-sm:placeholder:text-xs bg-[#dbdbdb] pl-5"
-              />
-              <br />
-              {message}
-            </section>
-          </section>
-          <section className="grid grid-cols-2  max-sm:grid-cols-1 mt-5 text-[#777777] text-[16px] font-bold gap-5">
-            <section>
-              <label htmlFor="Phone#">Phone Number *</label> <br />
-              <input
-                type="text"
-                name=""
-                id="Phone#"
-                placeholder="Enter your phone number"
-                className="w-full h-[50px] max-sm:placeholder:text-xs bg-[#dbdbdb] pl-5"
-              />{" "}
-              <br />
-              {message}
-            </section>
-            <section>
-              <label htmlFor="Age">Age *</label> <br />
-              <input
-                type="text"
-                name=""
-                id="Age"
-                placeholder="Enter your age"
-                className="w-full h-[50px] max-sm:placeholder:text-xs bg-[#dbdbdb] pl-5"
-              />{" "}
-              <br />
-              {message}
-            </section>
-          </section>
-        </section>
-        <section className="mt-5  text-[#777777] text-[16px] font-bold gap-5">
-          <label htmlFor="Email"> Email *</label>
-          <input
-            type="email"
-            id="Email"
-            placeholder="Enter your email address"
-            className="w-full h-[50px]  max-sm:placeholder:text-xs bg-[#dbdbdb] pl-5"
-          />{" "}
-          <br />
-          {message} <br />
-          <br />
-          <label htmlFor="Address">Address *</label>
-          <input
-            type="text"
-            id="Address"
-            placeholder="Enter your address"
-            className="w-full h-[50px]  max-sm:placeholder:text-xs bg-[#dbdbdb] pl-5"
-          />
-          {message}
-        </section>
-        <section className="grid grid-cols-2 gap-5  max-sm:grid-cols-1 text-[#777777] text-[16px] font-bold mt-5">
-          <section>
-            <label htmlFor="City"> City *</label>
-            <input
-              type="email"
-              id="City  "
-              placeholder="Enter your city"
-              className="w-full h-[50px]  max-sm:placeholder:text-xs bg-[#dbdbdb] pl-5"
-            />{" "}
-            <br />
-            {message}
-          </section>
-          <section>
-            <label htmlFor="Zip-Code">Zip Code *</label>
-            <input
-              type="text"
-              id="Zip-Code"
-              placeholder="Enter your zip code"
-              className="w-full h-[50px] max-sm:placeholder:text-xs bg-[#dbdbdb] pl-5"
+      <h1 className="text-[#ff4d30] font-extrabold text-xl uppercase mt-5">
+        Personal Information
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <section className="grid grid-cols-2 gap-5 px-3 max-sm:px-1 max-md:grid-cols-1  text-lg mt-5">
+          {inputFields.slice(0, 4).map((field, index) => (
+            <InputField
+              key={index}
+              label={field.label}
+              name={field.name}
+              placeholder={field.placeholder}
+              register={register}
+              errors={errors}
             />
-            {message}
-          </section>
+          ))}
         </section>
-        <section className="mt-5 text-[16px] font-sans font-bold text-[#565454]">
-          <input type="checkbox" id="check" />{" "}
-          <label htmlFor="check">Please send me latest news and updates</label>
+        <section className="flex flex-col px-3 text-lg mt-3 gap-5 max-sm:px-1">
+          {inputFields.slice(4, 6).map((field, index) => (
+            <InputField
+              key={index}
+              label={field.label}
+              name={field.name}
+              placeholder={field.placeholder}
+              register={register}
+              errors={errors}
+            />
+          ))}
         </section>
-        <section className=" pt-12 pr-5 mt-5  bg-[#dbdbdb] w-full h-[150px]">
-          <section className=" flex items-center justify-center ml-5">
-            <button className="bg-[#fe3516] text-white w-[250px] h-[50px] text-[20px] font-extrabold font-serif items shadow-lg rounded-md">
-              Reserve Now
-            </button>
-          </section>
+
+        <section className="grid grid-cols-2 gap-5 px-3 mt-3 text-lg max-sm:px-1  max-md:grid-cols-1">
+          {inputFields.slice(6, 8).map((field, index) => (
+            <InputField
+              key={index}
+              label={field.label}
+              name={field.name}
+              placeholder={field.placeholder}
+              register={register}
+              errors={errors}
+            />
+          ))}
         </section>
-      </section>
+
+        <section className="flex max-sm:items-center max-sm:justify-center max-sm:mx-3  mt-5">
+          <button
+            type="submit"
+            className="bg-[#fe3516] text-white w-[250px] h-[60px] text-[20px] mr-3 mt-5  max-sm:mr-0 max-sm:ml-0 max-sm:w-full  font-extrabold items shadow-lg ml-auto rounded-md hover:bg-[#ff5a3c] transition duration-300 ease-in-out"
+          >
+            Reserve Now
+          </button>
+        </section>
+      </form>
+
+      {openModal && (
+        <CompleteReservation
+          handleModal={handleModal}
+          userInformation={userInformation}
+          reservationDetails={reservationDetails}
+        />
+      )}
     </>
   );
 };
