08f4a5d254897bc5c39c069e62b5b19751743ea2
NARD <glenardrefamonte1@gmail.com>
2024-04-22 00:06:23 +0800

    import the schema and implement the zod resolvers and the react hook form

--- src/components/Booking/BookingCar.jsx
+++ src/components/Booking/BookingCar.jsx
@@ -2,15 +2,33 @@ import React, { useState } from "react";
 import { PiCarProfileBold } from "react-icons/pi";
 import { HiLocationMarker } from "react-icons/hi";
 import { type, pickUp, dropOf } from "./CarData.js";
-import Date from "./Date.jsx";
-
+import { AiOutlineCalendar } from "react-icons/ai";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { reserveCarSchema } from "../../Schema/ReserveCarSchema.js";
+import ReserveModal from "./ReserveModal.jsx";
 function BookingCar() {
-  const [selectedPickUp, setSelectedPickUp] = useState("");
-  const [selectedCar, setSelectedCar] = useState("");
-  const [selectedDropOff, setSelectedDropOff] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [data, setData] = useState(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(reserveCarSchema),
+    carType: "",
+    pickUp: "",
+    dropOff: "",
+    pickUpDate: "",
+    dropOffDate: "",
+  });
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const onSubmit = booking => {
+    showModal();
+    setData(booking);
   };
 
   return (
@@ -21,7 +39,7 @@ function BookingCar() {
             Book a car
           </h1>
 
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <section className="lg:flex max-sm:flex-col px-10 max-sm:px-5 font-[600]">
               <section className=" mb-5  sm:mr-2 w-full">
                 <div className="flex   items-center gap-3 mb-2 sm:mb-4">
@@ -31,11 +49,9 @@ function BookingCar() {
                 <select
                   className="select w-full  bg-white border-slate-300 border-2"
                   name="carType"
-                  value={selectedCar}
-                  required
-                  onChange={e => setSelectedCar(e.target.value)}
+                  {...register("carType")}
                 >
-                  <option value="" defaultValue>
+                  <option value="" defaultValue className="text-gray-500">
                     Select your car type
                   </option>
                   {type.map((item, index) => (
@@ -44,6 +60,11 @@ function BookingCar() {
                     </option>
                   ))}
                 </select>
+                {errors?.carType && (
+                  <span className="block text-red-400 text-sm ml-3  text-left  ">
+                    {errors?.carType?.message}
+                  </span>
+                )}
               </section>
 
               <section className=" mb-5  sm:mr-2 w-full">
@@ -53,11 +74,10 @@ function BookingCar() {
                 </div>
                 <select
                   className="select w-full  bg-white border-slate-300 border-2"
-                  name="carType"
-                  value={selectedPickUp}
-                  onChange={e => setSelectedPickUp(e.target.value)}
+                  name="pickUp"
+                  {...register("pickUp")}
                 >
-                  <option value="" defaultValue>
+                  <option value="" defaultValue className="text-gray-500">
                     Select pick up location
                   </option>
                   {pickUp.map((item, index) => (
@@ -66,6 +86,11 @@ function BookingCar() {
                     </option>
                   ))}
                 </select>
+                {errors?.pickUp && (
+                  <span className="block text-red-400 text-sm text-left  ml-3">
+                    {errors?.pickUp?.message}
+                  </span>
+                )}
               </section>
 
               <section className=" mb-5 sm:mr-2 w-full">
@@ -75,11 +100,10 @@ function BookingCar() {
                 </div>
                 <select
                   className="select w-full  bg-white border-slate-300 border-2"
-                  name="carType"
-                  value={selectedDropOff}
-                  onChange={e => setSelectedDropOff(e.target.value)}
+                  name="dropOff"
+                  {...register("dropOff")}
                 >
-                  <option value="" defaultValue>
+                  <option value="" defaultValue className="text-gray-500">
                     Select drop off location
                   </option>
                   {dropOf.map((item, index) => (
@@ -87,17 +111,64 @@ function BookingCar() {
                       {item}
                     </option>
                   ))}
-                </select>
+                </select>{" "}
+                {errors?.dropOff && (
+                  <span className="block text-red-400 text-sm text-left  ml-3">
+                    {errors?.dropOff?.message}
+                  </span>
+                )}
               </section>
             </section>
-            <Date
-              selectedPickUp={selectedPickUp}
-              selectedCar={selectedCar}
-              selectedDropOff={selectedDropOff}
-            />
+            <section className="lg:flex gap-3 font-[600] justify-between max-sm:flex-col  mb-10 px-10 max-sm:px-5 items-center mt-5   text-gray-900 ">
+              <section className="w-full  ">
+                <div className="flex items-center gap-3 max-sm:mx-2">
+                  <AiOutlineCalendar /> Pick-up Date
+                  <span className="text-orange-500"> *</span>
+                </div>
+                <input
+                  type="date"
+                  {...register("pickUpDate")}
+                  className="w-full  mt-3 h-[50px] bg-white border-slate-300 border-2  text-sm   font-bold text-black rounded-lg px-5"
+                />
+                {errors?.pickUpDate && (
+                  <span className="block text-red-400 text-sm text-left  ml-3">
+                    {errors?.pickUpDate?.message}
+                  </span>
+                )}
+              </section>
+              <section className="w-full font-[600] max-lg:mt-5">
+                <div className="flex items-center gap-3 max-sm:mx-2 ">
+                  <AiOutlineCalendar /> Drop off Date
+                  <span className="text-orange-500"> *</span>
+                </div>
+                <input
+                  type="date"
+                  {...register("dropOffDate")}
+                  className="w-full  mt-3 h-[50px] bg-white border-slate-300 border-2  font-bold text-black rounded-lg text-sm px-5"
+                />
+                {errors?.dropOffDate && (
+                  <span className="block text-red-400 text-sm text-left ml-3">
+                    {errors?.dropOffDate?.message}
+                  </span>
+                )}
+              </section>
+              <section className="flex w-full mt-9 items-center">
+                <button
+                  type="submit"
+                  className="w-full  h-[50px] bg-orange-500 text-white  font-bold text-[20px] rounded-md shadow-lg"
+                >
+                  Search
+                </button>
+              </section>
+            </section>
           </form>
         </section>
       </section>
+      <ReserveModal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        data={data}
+      />
     </>
   );
 }
