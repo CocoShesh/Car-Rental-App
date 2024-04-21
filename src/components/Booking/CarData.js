0eb7f82b1f6bee7a6cf1a03713b11101dfd9d61c
NARD <glenardrefamonte1@gmail.com>
2024-04-22 00:06:42 +0800

    Added mock data for the list of the cars

--- src/components/Booking/CarData.js
+++ src/components/Booking/CarData.js
@@ -12,3 +12,35 @@ const pickUp = ["Sucat", "Taguig", "Alabang", "Bicutan", "Makati"];
 const dropOf = ["Taguig", "Sucat", "Bicutan", "Makati", "Alabang"];
 
 export { type, pickUp, dropOf };
+
+export const Cars = [
+  { carName: "Audi A1S-Line", src: "../images/Audi.jpg" },
+  { carName: "Vw Golf 6", src: "../images/Golf.jpg" },
+  { carName: "Toyota Camry", src: "../images/toyotacamry.jpg" },
+  { carName: " BMW 320 ModernLine", src: "../images/ModernLine.jpg" },
+  { carName: "Mercedes-Benz GLk", src: "../images/Mercedes.jpg" },
+  { carName: "VW Passat CC", src: "../images/Passat.jpg" },
+];
+
+export const inputFields = [
+  {
+    label: "First Name",
+    name: "firstName",
+    placeholder: "Enter your first name",
+  },
+  {
+    label: "Last Name",
+    name: "lastName",
+    placeholder: "Enter your last name",
+  },
+  {
+    label: "Phone Number",
+    name: "phoneNumber",
+    placeholder: "Enter your phone number",
+  },
+  { label: "Age", name: "age", placeholder: "Enter your age" },
+  { label: "Email", name: "email", placeholder: "Enter your email address" },
+  { label: "Address", name: "address", placeholder: "Enter your address" },
+  { label: "City", name: "city", placeholder: "Enter your city" },
+  { label: "Zip Code", name: "zipCode", placeholder: "Enter your zip code" },
+];
