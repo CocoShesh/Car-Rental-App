0ef9a26c76159e66ebf946c35d29002bd04572e4
NARD <glenardrefamonte1@gmail.com>
2024-04-23 00:51:18 +0800

    Added style to remove seleting content

--- src/Pages/Testimonials/Review.jsx
+++ src/Pages/Testimonials/Review.jsx
@@ -6,7 +6,7 @@ const Review = () => {
   return (
     <>
       <HeaderPage>
-        <section className="absolute  mx-10 top-[140px] 2xl:w-[1500px]  w-full px-8 ">
+        <section className="absolute  mx-10 top-[140px] 2xl:w-[1500px]  w-full px-8  select-none">
           <h1 className="text-[42px] ">Testimonials</h1>
           <p>
             <Link to="/">
