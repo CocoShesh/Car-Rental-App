878c8859479ceb8f0227b8540513e342c6961d31
NARD <glenardrefamonte1@gmail.com>
2024-04-23 00:51:51 +0800

    Added style to remove seleting content

--- src/Pages/header/HeaderPage.jsx
+++ src/Pages/header/HeaderPage.jsx
@@ -5,7 +5,7 @@ const HeaderPage = ({ children }) => {
     <>
       <section className=" relative  h-[250px] w-full  bg-header-overlay bg-cover">
         <section className="absolute  z-10 top-0 left-0 w-full h-[250px]   bg-[#f3f3f3d8]"></section>
-        <section className=" absolute z-20 top-0 w-full flex items-center justify-center ">
+        <section className=" absolute z-20 top-0 w-full flex items-center justify-center  select-none">
           <Header />
           {children}
         </section>
