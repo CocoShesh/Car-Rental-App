fa327068e8167663aa1941dede7750737aa00f7c
NARD <glenardrefamonte1@gmail.com>
2024-04-23 00:52:30 +0800

    Added style to remove seleting content

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Banner = () => {
   return (
     <>
-      <section className="bg-[#2d2d2d] relative z-10 w-full h-[250px]  py-10 mb-20 flex flex-col items-center text-center justify-center max-sm:w-full max-sm:pl-5 max-sm:h-auto max-sm:text-center  ">
+      <section className="bg-[#2d2d2d] relative z-10 w-full h-[250px]  py-10 mb-20 flex flex-col  select-none items-center text-center justify-center max-sm:w-full max-sm:pl-5 max-sm:h-auto max-sm:text-center  ">
         <h1 className="text-white text-[48px] mb-2 max-sm:text-[34px] ">
           Save big with our cheap car rental!
         </h1>
